50e843ea106a4e2b595b4b0fe75547f5b11eef02
demitryedwards <dxyz773@gmail.com>
2023-04-10 23:05:29 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(collection, str) {
+  const match = collection.filter(
+    (el) => el.toLowerCase() === str.toLowerCase()
+  );
+  return match;
+}
+
+// findMatching(["Marty", "Mary", "mary", "marty"], "Marty");
+function fuzzyMatch(collection, str) {
+  const match = collection.filter((el) => el.startsWith(str));
+  return match;
+}
+
+function matchName(collection, str) {
+  const match = collection.filter((el) => el.name === str);
+  return match;
+}
